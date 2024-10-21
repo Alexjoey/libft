6cb48ba959882e64a86a0b3b71ae55bdb776d7c2
Alex Mylle <alexm@live.be>
2024-10-21 06:54:58 +0200

    added proper strjoin

--- src/libft/ft_strjoin.c
+++ src/libft/ft_strjoin.c
@@ -21,18 +21,12 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	res = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
 	if (!res)
 		return (NULL);
-	i = 0;
-	while (s1[i])
-	{
+	i = -1;
+	while (s1 && s1[++i])
 		res[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (s2[j])
-	{
+	j = -1;
+	while (s2[++j])
 		res[i + j] = s2[j];
-		j++;
-	}
 	res[i + j] = '\0';
 	return (res);
 }
@@ -43,19 +37,14 @@ char	*ft_strjoinfree(char *str, char const *buff)
 	int		i;
 	char	*res;
 
-	if (!str)
-		str = ft_calloc(1, 1);
-	if (!str || !buff)
-		return (NULL);
 	res = (char *)malloc((ft_strlen(str) + ft_strlen(buff) + 1) * sizeof(char));
 	if (!res)
 		return (NULL);
 	i = -1;
-	if (str)
-		while (str[++i])
-			res[i] = str[i];
+	while (str && str[++i])
+		res[i] = str[i];
 	j = -1;
-	while (buff[++j])
+	while (buff && buff[++j])
 		res[i + j] = buff[j];
 	res[i + j] = '\0';
 	free (str);
